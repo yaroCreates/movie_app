f1eb2e24f92c8380bbe91f671d447d64e887a7f2
James Yaro <onuhjamesyaro@gmail.com>
2022-01-22 21:19:53 +0100

    designed the header of the mainbar

--- src/components/Mainbar.js
+++ src/components/Mainbar.js
@@ -5,10 +5,15 @@ import Moviecard from './Moviecard';
 function Mainbar({ items }) {
 
     return (
-        <div className='movie'>
+        <div className='mainbar'>
+            <div className='mainbar_header'>
+                
+            </div>
+            <div className='movie'>
             {items.map(item => (
                 <Moviecard key={item.id} item={item}/>
             ))}
+        </div>
         </div>
     )
 
