f46b9c45f90d99dd3245b4602497f7a4ccf6d301
James Yaro <onuhjamesyaro@gmail.com>
2022-01-23 17:31:42 +0100

    Mainbar header design

--- src/components/Mainbar.js
+++ src/components/Mainbar.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './Mainbar.css'
 import Moviecard from './Moviecard';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Mainbar({ items }) {
 
     return (
         <div className='mainbar'>
             <div className='mainbar_header'>
-                
+                <div className='mainbar_search'>
+                    <SearchIcon className='mainbar_search_icon' />
+                    <input className='mainbar_search_input' placeholder='Search movies'/>
+                    <button className='mainbar_search_btn'>Search</button>
+                </div>
             </div>
             <div className='movie'>
             {items.map(item => (
