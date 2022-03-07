0af25b4dae91ada8449993eda367e64021712dc4
James Yaro <onuhjamesyaro@gmail.com>
2022-03-07 12:57:10 +0100

    Sidebar icons

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import './Sidebar.css'
+// import WhatshotIcon from '@material-ui/icons/Whatshot';
+// import LocalMoviesIcon from '@material-ui/icons/LocalMovies';
+
+
+import { LocalMoviesOutlined, RateReviewOutlined, WhatshotOutlined, LiveTvOutlined } from '@material-ui/icons'
 
 function Sidebar({ trending, popular, top_rated, tv_show }) {
 
@@ -22,10 +27,22 @@ function Sidebar({ trending, popular, top_rated, tv_show }) {
                 <h1 onClick={popular} className='sidebar_logo'>MovieApp</h1>
             </div>
             <div id="sidebar_ID" className='sidebar_links'>
-                <a onClick={popular} className='sidebar_link active'>Popular Movies</a>
-                <a onClick={trending} className='sidebar_link'>Trending</a>
-                <a onClick={top_rated} className='sidebar_link'>Top Rated</a>
-                <a onClick={tv_show} className='sidebar_link'>TV Shows</a>
+                <div className='sidebar_class'>
+                    <a onClick={popular} className='sidebar_link active'>Popular Movies</a>
+                    < LocalMoviesOutlined fontSize='small'/>
+                </div>
+                <div className='sidebar_class'>
+                    <a onClick={trending} className='sidebar_link'>Trending</a>
+                    <  WhatshotOutlined fontSize='small'/>
+                </div>
+                <div className='sidebar_class'>
+                    <a onClick={top_rated} className='sidebar_link'>Top Rated</a>
+                    < RateReviewOutlined fontSize='small'/>
+                </div>
+                <div className='sidebar_class'>
+                    <a onClick={tv_show} className='sidebar_link'>TV Shows</a>
+                    < LiveTvOutlined fontSize='small'/>
+                </div>
             </div>
         </div>
 
